7e702e1085e985901666e5a966253d4d48f4c210
Sonia Pargas <soniapargas@gmail.com>
2021-02-25 16:20:35 -0600

    this is the third_function

--- test/example.py
+++ test/example.py
@@ -4,5 +4,9 @@ def a_new_function():
 def second_function():
   return "\nwhats up again"
 
+def third_function():
+    return "busy"
+
 print(a_new_function())
 print(second_function())
+print(third_function())
